d9ff19efbabc63e6c1c80a44b122df3ed0c2722c
Brandon Mulcahy <brandon@lightcones.net>
2020-05-07 20:08:18 -0400

    Prevent save scum abuse in Oracles controller

--- bizhawk-co-op/ramcontroller/Oracles.lua
+++ bizhawk-co-op/ramcontroller/Oracles.lua
@@ -6,6 +6,8 @@ local seasons_addrs = {
 	wNetTreasureIn = 0xcbfb,
 	wNetPlayerOut = 0xcbfd,
 	wNetTreasureOut = 0xcbfe,
+	wActiveGroup = 0xcc49,
+	wActiveRoom = 0xcc4c,
 }
 local ages_addrs = {
 }
@@ -31,6 +33,7 @@ end
 
 local this_player = memory.readbyte(addrs.multiPlayerNumber)
 local items_in = {}
+local rooms_out = {} -- to prevent save scum abuse
 local oracles_ram = {} -- exports RAM controller interface
 
 -- Gets a message to send to the other player of new changes
@@ -53,14 +56,24 @@ function oracles_ram.getMessage()
 	-- buffered treasure out? send and clear it
 	local out_player = memory.readbyte(addrs.wNetPlayerOut)
 	if out_player ~= 0 then
+		-- get and clear vars
 		local out_id = memory.readbyte(addrs.wNetTreasureOut)
 		local out_param = memory.readbyte(addrs.wNetTreasureOut + 1)
-		message["m"] = {out_player, out_id, out_param}
 		memory.writebyte(addrs.wNetPlayerOut, 0)
 		memory.writebyte(addrs.wNetTreasureOut, 0)
 		memory.writebyte(addrs.wNetTreasureOut + 1, 0)
-		console.log(string.format("sent item to P%d: {%02x, %02x}",
-			out_player, out_id, out_param))
+
+		-- send message if room's item hasn't been sent before
+		local room = memory.readbyte(addrs.wActiveGroup) * 0x100 +
+			memory.readbyte(addrs.wActiveRoom)
+		if rooms_out[room] then
+			console.log(string.format("item from room %04x already sent", room))
+		else
+			rooms_out[room] = true
+			message["m"] = {out_player, out_id, out_param}
+			console.log(string.format("sent item to P%d: {%02x, %02x}",
+				out_player, out_id, out_param))
+		end
 	end
 
 	-- return the message if it has content
